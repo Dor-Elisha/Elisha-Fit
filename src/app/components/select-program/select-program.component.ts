ae16e79d7e468189f190718de702d7fa02f84769
Dor-Elisha <dorelisha21@gmail.com>
2025-07-08 23:13:47 +0300

    feat: enhance SelectProgramComponent with exercise selection and display for each day

--- src/app/components/select-program/select-program.component.ts
+++ src/app/components/select-program/select-program.component.ts
@@ -3,32 +3,31 @@ import { GeneralService } from 'src/app/services/general.service';
 import { ExerciseService } from '../../services/exercise.service';
 import * as _ from 'lodash';
 
-
-
-interface Exercise {
-  name: string;
-  muscle_group: string;
-  reps: string;
-  times: number;
-  rest_between: string;
-}
-
 @Component({
   selector: 'app-select-program',
   templateUrl: './select-program.component.html',
   styleUrls: ['./select-program.component.scss']
 })
 export class SelectProgramComponent {
-  constructor(private gs: GeneralService, private exerciseService: ExerciseService) { }
+  constructor(private gs: GeneralService, public exerciseService: ExerciseService) { }
   newProgram:any;
+
   _=_;
   containerOpen = false;
-
+  selectedDay = 'Sunday';
   createNewProgram(): void {
     this.newProgram = {
       name: '',
       description: '',
-      exercises: [],
+      exercises: {
+        Sunday: { programs: [] },
+        Monday: { programs: [] },
+        Tuesday: { programs: [] },
+        Wednesday: { programs: [] },
+        Thursday: { programs: [] },
+        Friday: { programs: [] },
+        Saturday: { programs: [] }
+      },
       duration: 0,
       level: '',
       category: '',
@@ -48,8 +47,14 @@ export class SelectProgramComponent {
 
 
   onDayClick(day: string) {
+    this.selectedDay = day;
     this.containerOpen = true;
     // handle day selection logic if needed
   }
 
+  addExercise = (exercise: any) => {
+    if (this.newProgram && this.newProgram.exercises && this.selectedDay) {
+      this.newProgram.exercises[this.selectedDay].programs.push(exercise);
+    }
+  }
 }
